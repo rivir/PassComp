88d5f97ecb1e89d17b6a6f3a712d116ef140b8ac
rivir <rivir@users.noreply.github.com>
2021-04-26 19:30:18 -0700

    function adoption

--- counter.js
+++ counter.js
@@ -1,172 +1,26 @@
 window.onload = function () {
-    var button = document.getElementById("offense-f"),
-        count = 0;
-    button.onclick = function () {
-        count += 1;
-        button.innerHTML = "Off 1/3<br>" + count;
-    };
-    button.oncontextmenu = function () {
-        count -= 1;
-        button.innerHTML = "Off 1/3<br>" + count;
-        return false;
-    };
-
-    var button2 = document.getElementById("offtrans-f"),
-        count2 = 0;
-    button2.onclick = function () {
-        count2 += 1;
-        button2.innerHTML = "Trans<br>" + count2;
-    };
-    button2.oncontextmenu = function () {
-        count2 -= 1;
-        button2.innerHTML = "Trans<br>" + count2;
-        return false;
-    };
-
-    var button3 = document.getElementById("middle-f"),
-        count3 = 0;
-    button3.onclick = function () {
-        count3 += 1;
-        button3.innerHTML = "Mid 1/3<br>" + count3;
-    };
-    button3.oncontextmenu = function () {
-        count3 -= 1;
-        button3.innerHTML = "Mid 1/3<br>" + count3;
-        return false;
-    };
-
-    var button4 = document.getElementById("deftrans-f"),
-        count4 = 0;
-    button4.onclick = function () {
-        count4 += 1;
-        button4.innerHTML = "Trans<br>" + count4;
-    };
-    button4.oncontextmenu = function () {
-        count4 -= 1;
-        button4.innerHTML = "Trans<br>" + count4;
-        return false;
-    };
-
-    var button5 = document.getElementById("defense-f"),
-        count5 = 0;
-    button5.onclick = function () {
-        count5 += 1;
-        button5.innerHTML = "Def 1/3<br>" + count5;
-    };
-    button5.oncontextmenu = function () {
-        count5 -= 1;
-        button5.innerHTML = "Def 1/3<br>" + count5;
-        return false;
-    };
-
-    var button10 = document.getElementById("offense-s"),
-        count10 = 0;
-    button10.onclick = function () {
-        count10 += 1;
-        button10.innerHTML = "Off 1/3<br>" + count10;
-    };
-    button10.oncontextmenu = function () {
-        count10 -= 1;
-        button10.innerHTML = "Off 1/3<br>" + count10;
-        return false;
-    };
-
-    var button12 = document.getElementById("offtrans-s"),
-        count12 = 0;
-    button12.onclick = function () {
-        count12 += 1;
-        button12.innerHTML = "Trans<br>" + count12;
-    };
-    button12.oncontextmenu = function () {
-        count12 -= 1;
-        button12.innerHTML = "Trans<br>" + count12;
-        return false;
-    };
-
-    var button13 = document.getElementById("middle-s"),
-        count13 = 0;
-    button13.onclick = function () {
-        count13 += 1;
-        button13.innerHTML = "Mid 1/3<br>" + count13;
-    };
-    button13.oncontextmenu = function () {
-        count13 -= 1;
-        button13.innerHTML = "Mid 1/3<br>" + count13;
-        return false;
-    };
-
-    var button14 = document.getElementById("deftrans-s"),
-        count14 = 0;
-    button14.onclick = function () {
-        count14 += 1;
-        button14.innerHTML = "Trans<br>" + count14;
-    };
-    button14.oncontextmenu = function () {
-        count14 -= 1;
-        button14.innerHTML = "Trans<br>" + count14;
-        return false;
-    };
-
-    var button15 = document.getElementById("defense-s"),
-        count15 = 0;
-    button15.onclick = function () {
-        count15 += 1;
-        button15.innerHTML = "Def 1/3<br>" + count15;
-    };
-    button15.oncontextmenu = function () {
-        count15 -= 1;
-        button15.innerHTML = "Def 1/3<br>" + count15;
-        return false;
-    };
-
-
-    var button21 = document.getElementById("throw-f"),
-        count21 = 0;
-    button21.onclick = function () {
-        count21 += 1;
-        button21.innerHTML = "Throws-<br>" + count21;
-    };
-    button21.oncontextmenu = function () {
-        count21 -= 1;
-        button21.innerHTML = "Throws-<br>" + count21;
-        return false;
-    };
-
-    var button22 = document.getElementById("throw-s"),
-        count22 = 0;
-    button22.onclick = function () {
-        count22 += 1;
-        button22.innerHTML = "Throws+<br>" + count22;
-    };
-    button22.oncontextmenu = function () {
-        count22 -= 1;
-        button22.innerHTML = "Throws+<br>" + count22;
-        return false;
-    };
-
-    var button23 = document.getElementById("reset"),
-        count23 = 0;
-    button23.onclick = function () {
-        count23 += 1;
-        button23.innerHTML = "Reset<br>" + count23;
-    };
-    button23.oncontextmenu = function () {
-        count23 -= 1;
-        button23.innerHTML = "Reset<br>" + count23;
-        return false;
-    };
-
-
     PassArray = new Array();
-    //PassArray[37] = 0;
-    ButtonProcess("gk-f", "GoalKicks-<br>", 30);
-    ButtonProcess("gk-s", "GoalKicks+<br>", 31);
-    ButtonProcess("shots-f", "Shots-<br>", 32);
-    ButtonProcess("shots-s", "Shots+<br>", 33);
-    ButtonProcess("clear-f", "clear<br>", 34);
-    ButtonProcess("freekick", "free kicks<br>", 35);
-    ButtonProcess("1v1-f", "1v1-<br>", 36);
-    ButtonProcess("1v1-s", "1v1+<br>", 37);
+    ButtonProcess("offense-f", "Off 1/3<br>", 0);
+    ButtonProcess("offtrans-f", "Trans<br>", 1);
+    ButtonProcess("middle-f", "Mid 1/3<br>", 2);
+    ButtonProcess("deftrans-f", "Trans<br>", 3);
+    ButtonProcess("defense-f", "Def 1/3<br>", 4);
+    ButtonProcess("offense-s", "Off 1/3<br>", 5);
+    ButtonProcess("offtrans-s", "Trans<br>", 6);
+    ButtonProcess("middle-s", "Mid 1/3<br>", 7);
+    ButtonProcess("deftrans-s", "Trans<br>", 8);
+    ButtonProcess("defense-s", "Def 1/3<br>", 9);
+    ButtonProcess("throw-f", "Throws-<br>", 10);
+    ButtonProcess("throw-s", "Throws+<br>", 11);
+    ButtonProcess("reset", "Reset<br>", 12);
+    ButtonProcess("gk-f", "GoalKicks-<br>", 13);
+    ButtonProcess("gk-s", "GoalKicks+<br>", 14);
+    ButtonProcess("shots-f", "Shots-<br>", 15);
+    ButtonProcess("shots-s", "Shots+<br>", 16);
+    ButtonProcess("clear-f", "clear<br>", 17);
+    ButtonProcess("freekick", "free kicks<br>", 18);
+    ButtonProcess("1v1-f", "1v1-<br>", 19);
+    ButtonProcess("1v1-s", "1v1+<br>", 20);
 
     function ButtonProcess(elementname, buttonname, index) {
         var buttonx = document.getElementById(elementname);
